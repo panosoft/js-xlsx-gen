91a90d55875be5e3aa0ea704327bac79efe6e2c0
Charles Scalfani <charles@panosoft.com>
2016-02-03 14:38:47 -0800

    Removed debug code

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,6 @@ const getStdin = require('get-stdin');
 co(function * () {
 	try {
 		const input = JSON.parse(yield getStdin());
-		console.error(input.xlsGenOptions)
 		const xlsGen = require('../lib')(input.xlsGenOptions);
 		const workbook = xlsGen.generate(input.spreadsheet, input.generateOptions);
 		process.stdout.write(xlsGen.write(workbook, {bookType:'xlsx', type:'binary'}), 'binary');
