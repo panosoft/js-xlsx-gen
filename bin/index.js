1f958343b5994cac2e5901da5d6ef880f3ec7e7d
Charles Scalfani <charles@panosoft.com>
2016-04-26 17:00:55 -0700

    Support sheetOrder to counter the reordering due to JSON stringify/parse

--- bin/index.js
+++ bin/index.js
@@ -2,11 +2,19 @@
 
 const co = require('co');
 const getStdin = require('get-stdin');
-
+const R = require('ramda');
 
 co(function * () {
 	try {
 		const input = JSON.parse(yield getStdin());
+		// if sheetOrder is specified, then reorder the keys
+		// this is a problem since stringify/parse doesn't guarantee key order
+		if (input.sheetOrder) {
+			input.spreadsheet = R.reduce((acc, key) => {
+				acc[key] = input.spreadsheet[key];
+				return acc;
+			}, {}, input.sheetOrder);
+		}
 		const xlsGen = require('../lib')(input.xlsGenOptions);
 		const workbook = xlsGen.generate(input.spreadsheet, input.generateOptions);
 		process.stdout.write(xlsGen.write(workbook, {bookType:'xlsx', type:'binary'}), 'binary');
